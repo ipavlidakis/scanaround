fdc509e9deb1cd66b38b79ad67484ef33908bfd7
Ilias Pavlidakis <iPavlidakis@Iliass-MacBook-Pro.local>
2016-07-10 22:25:59 +0100

    Fix for web server

--- index.js
+++ index.js
@@ -1,44 +1,50 @@
-var Firebase = require('firebase');
-var elasticsearch = require('elasticsearch');
+var http = require('http');
+ var fs = require('fs');
+ var path = require('path');
 
-var config = {
-    firebaseUrl: 'https://scanaround-42693.firebaseio.com/',
-    elasticSearchUrl: 'https://pacific-refuge-11944.herokuapp.com/'
-}
-var rootRef = new Firebase(config.firebaseUrl);
+ http.createServer(function (request, response) {
 
-var client = new elasticsearch.Client({
-    host: config.elasticSearchUrl
-});
+    console.log('request starting for ');
+    console.log(request);
 
-var usersRef = rootRef.child('users');
+    var filePath = '.' + request.url;
+    if (filePath == './')
+        filePath = './index.html';
 
-usersRef.on('child_added', upsert);
-usersRef.on('child_changed', upsert);
-usersRef.on('child_removed', remove);
+    console.log(filePath);
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+    }
 
-function upsert(snapshot){
-    client.index({
-        index: 'firebase',
-        type: 'users',
-        id: snapshot.key(),
-        body: snapshot.val()
-    }, function(err, response){
-        if(err){
-            console.log("Error indexing user : " + error);
+    path.exists(filePath, function(exists) {
+
+        if (exists) {
+            fs.readFile(filePath, function(error, content) {
+                if (error) {
+                    response.writeHead(500);
+                    response.end();
+                }
+                else {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                }
+            });
         }
-    })
-
-}
-
-function remove(snapshot){
-    client.delete({
-        index: 'firebase',
-        type: 'users',
-        id: snapshot.key()
-    }, function(error, response){
-        if(error){
-            console.log("Error deleting user : " + error);
+        else {
+            response.writeHead(404);
+            response.end();
         }
     });
-}
+
+ }).listen(process.env.PORT || 5000);
+
+ console.log('Server running at http://127.0.0.1:' + process.env.PORT);
+
+Any idea ?
